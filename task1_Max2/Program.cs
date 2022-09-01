9c89ecd248d9b8d0f55ca4a5bc2effa6e903e60b
Alevtina <_@email.comgit>
2022-09-01 07:54:39 +0300

    Add task1 max iz 2 chisel

---
+++ task1_Max2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите число a и нажмите Enter ");
+string First = Console.ReadLine();
+int FirstA = Convert.ToInt32(First);
+Console.Write("Введите число b и нажмите Enter ");
+string Second = Console.ReadLine();
+int SecondB = Convert.ToInt32(Second);
+
+int max = FirstA;
+if (FirstA > SecondB)
+ { 
+    max = FirstA;
+ }
+else 
+  {  
+    max = SecondB;
+  }
+  Console.Write("Максималььное число ");
+  Console.Write(max);
